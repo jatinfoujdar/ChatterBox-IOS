8da24d2381858001c36fbea378573dc0627cabb0
Jatin Foujdar <jatin321001@gmail.com>
2025-01-11 19:22:07 +0530

    Initial Commit

---
+++ ChatterBox/ChatterBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterBoxApp.swift
+//  ChatterBox
+//
+//  Created by jatin foujdar on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatterBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
