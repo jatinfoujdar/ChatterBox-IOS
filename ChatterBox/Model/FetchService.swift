84dbb00f62032c7f3db7be7983f5021189212358
Jatin Foujdar <jatin321001@gmail.com>
2025-01-15 15:14:39 +0530

    decode data commit

--- ChatterBox/Model/FetchService.swift
+++ ChatterBox/Model/FetchService.swift
@@ -14,6 +14,20 @@ struct FetchService{
         let quoteUrl = baseURL.appending(path: "quotes/random")
         let fetchUrl = quoteUrl.appending(queryItems: [URLQueryItem(name: "production", value: show)])
         
+        //fetch data
+        let (data, response) = try await URLSession.shared.data(from: fetchUrl)
+        
+        //handle response
+        guard let response = response as? HTTPURLResponse , response.statusCode == 200 else{
+            throw FetchError.badResponse
+        }
+        
+        //decode data
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
+                
+        
+        //return quote
+        return quote
     }
     
 }
